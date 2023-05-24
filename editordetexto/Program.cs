7d904e8c0d433c6fddbdf9afa0c0e36e36ec9d1d
philipheferreira <philipheferreira@gmail.com>
2023-05-24 10:23:38 -0300

    update estrutura

--- editordetexto/Program.cs
+++ editordetexto/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+
+namespace EditordeTexto { 
+    class Program {  
+        static void Main(string[] args) {
+            Menu();
+        }
+
+        static void Menu() {
+
+            int menuEditordeTexto;
+
+            Console.Clear();
+            Console.WriteLine("Digite qual tarefa voce gostaria de realizar a baixo");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+
+            menuEditordeTexto = Convert.ToInt32(Console.ReadLine());
+
+            switch (menuEditordeTexto) {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir() { 
+        
+        }
+
+        static void Editar() { 
+        
+        }
+    }
+}
